9a2c0be3c989804d98baf38b3024a3fa7e7c021a
yashpurwar1 <yashpurwar.yashpurwar@gmail.com>
2021-09-28 09:14:05 +0530

    Feat: Added swagger

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 require("dotenv").config();
 // create express app
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // define a simple route
 app.get('/', (req, res) => {
