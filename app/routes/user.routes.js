cc79fc903bd98fdb83afc59a539da57cde9507dd
yashpurwar1 <yashpurwar.yashpurwar@gmail.com>
2021-10-12 12:32:22 +0530

    Refactor: HTTP verb in getNote api

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -14,6 +14,6 @@ module.exports=(app) =>{
     app.post('/forgotPassword', controller.forgotPassword);
     app.put('/resetPassword', helper.verifyToken ,controller.resetPassword);
     app.post('/createNote', helper.verifyToken, noteController.createNote);
-    app.post('/getNote', helper.verifyToken, noteController.getNote);
+    app.get('/getNote', helper.verifyToken, noteController.getNote);
     app.get('/getNoteById/:noteId', helper.verifyToken, noteController.getNoteById);
 }
